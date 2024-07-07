c7b8043289589e31bfa9b55196d310bd9c59c23d
obaidmuneer <obaidmuneer55@gmail.com>
2024-07-07 19:47:00 +0500

    code formatted

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read():
-    return {
-        "message": "Hello World"
-    }
+    return {"message": "Hello World"}
+
     
 @app.get("/items/{item}")
-def read(item,q=None):
-    return {
-        "message": "success",
-        "data":{
-            "id":item,
-            "q":q
-        }
-    }
+def read(item, q=None):
+    return {"message": "success", "data": {"id": item, "q": q}}
