8c5acc2ebb44f7c57f52d9b0e336bbedef406839
obaidmuneer <obaidmuneer55@gmail.com>
2024-07-13 18:05:24 +0500

    feat: add add category api and protected with admin auth

--- app/routers/category_router.py
+++ app/routers/category_router.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException
 from psycopg import Connection
 from pydantic import BaseModel, EmailStr
-from app.dependecies import DBDep, get_id
+from app.dependecies import AdminDep, DBDep, get_id
 from app.db import get_db
 from psycopg.rows import class_row
 
@@ -15,6 +15,10 @@ class CategoryDB(BaseModel):
     name: str
     created_at: datetime
     updated_at: datetime
+
+
+class CategoryReq(BaseModel):
+    name: str
 
 
 @router.get("/")
@@ -31,5 +35,24 @@ def category(id: int, conn: DBDep):
         record: CategoryDB = curr.execute(
             "select * from categories where id = %s", [id]
         ).fetchone()
+        print(record)
+        return record
+
+
+@router.post("/")
+def add_category(category: CategoryReq, conn: DBDep, is_admin: AdminDep):
+    print(category.name)
+    with conn.cursor(row_factory=class_row(CategoryDB)) as curr:
+        existing_record: CategoryDB = curr.execute(
+            "SELECT * FROM categories WHERE name = %s", [category.name]
+        ).fetchone()
+        print(existing_record)
+        if existing_record:
+            raise HTTPException(status_code=400, detail="category already exists")
+
+        record: CategoryDB = curr.execute(
+            "insert into categories (name) values (%s) returning *", [category.name]
+        ).fetchone()
+        conn.commit()
         print(record)
         return record
